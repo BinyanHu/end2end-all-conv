40fd6689387e9f7118402f9c1496de2a0e3d06a6
Binyan Hu <hubianhb@gmail.com>
2020-04-27 13:05:34 +0800

    reconfig path trainig

--- ddsm_train/train_image_clf_ddsm.sh
+++ ddsm_train/train_image_clf_ddsm.sh
@@ -3,24 +3,24 @@
 TRAIN_DIR="data/curated_breast_imaging_ddsm/train_dat_mod/train"
 VAL_DIR="data/curated_breast_imaging_ddsm/train_dat_mod/val"
 TEST_DIR="data/curated_breast_imaging_ddsm/train_dat_mod/test"
-#RESUME_FROM="saved_model/ddsm/3cls_best_model.h5"
-BEST_MODEL="saved_model/ddsm/3cls_best_model.h5"
-FINAL_MODEL="saved_model/ddsm/3cls_final_model.h5"
+#RESUME_FROM="saved_model/ddsm_patch/5cls_best_model.h5"
+BEST_MODEL="saved_model/ddsm_patch/5cls_best_model.h5"
+FINAL_MODEL="saved_model/ddsm_patch/5cls_final_model.h5"
 # FINAL_MODEL="NOSAVE"
 
-export NUM_CPU_CORES=8
+export NUM_CPU_CORES=4
 
 python ddsm_train/patch_clf_train.py \
-	--img-size 256 256 \
+    --img-size 256 256 \
     --img-scale 255.0 \
-	--featurewise-center \
+    --featurewise-center \
     --featurewise-mean 59.6 \
     --equalize-hist \
-	--batch-size 64 \
+    --batch-size 64 \
     --train-bs-multiplier 0.5 \
-	--augmentation \
-	--class-list background mass_mal mass_ben \
-	--nb-epoch 10 \
+    --augmentation \
+    --class-list background  calc_ben  calc_mal  mass_ben  mass_mal \
+    --nb-epoch 10 \
     --top-layer-epochs 5 \
     --all-layer-epochs 15 \
     --net resnet50 \
@@ -39,15 +39,15 @@ python ddsm_train/patch_clf_train.py \
     --inp-dropout 0.0 \
     --hidden-dropout 0.5 \
     --hidden-dropout2 0.0 \
-    --init-learningrate 0.01 \
+    --init-learningrate 0.001 \
     --top-layer-multiplier 0.01 \
     --all-layer-multiplier 0.0001 \
-	--lr-patience 2 \
-	--es-patience 5 \
-	--no-resume-from \
-	--auto-batch-balance \
+    --lr-patience 2 \
+    --es-patience 5 \
+    --no-resume-from \
+    --auto-batch-balance \
     --pos-cls-weight 1.0 \
-	--neg-cls-weight 1.0 \
-	--best-model $BEST_MODEL \
-	--final-model $FINAL_MODEL \
-	$TRAIN_DIR $VAL_DIR $TEST_DIR	
+    --neg-cls-weight 1.0 \
+    --best-model $BEST_MODEL \
+    --final-model $FINAL_MODEL \
+    $TRAIN_DIR $VAL_DIR $TEST_DIR    
