3476b62656c6e47bc612d9971d8a46599fa7fc75
Binyan Hu <hubianhb@gmail.com>
2020-05-13 18:00:30 +0800

    fix non ascii

--- dm_image.py
+++ dm_image.py
@@ -1222,7 +1222,7 @@ class DMDirectoryIterator(Iterator):
             batch_x[i] = x
             
             # print for debugging
-            print x.mean(), "±", x.std()
+            print x.mean(), "土", x.std()
 
         # optionally save augmented images to disk for debugging purposes
         if self.save_to_dir:
