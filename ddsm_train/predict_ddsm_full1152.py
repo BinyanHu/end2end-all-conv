3745300604e3bf8d39254146c55ab751d1079a26
Binyan Cloud <hubianhb@gmail.com>
2020-05-16 14:55:22 +1000

    finish data debugging

--- ddsm_train/predict_ddsm_full1152.py
+++ ddsm_train/predict_ddsm_full1152.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 from keras.models import load_model
+import numpy as np
 
 from dm_image import read_img_for_pred
 
@@ -17,15 +18,17 @@ resume_from = ARGS.resume_from
 rescale_factor = 0.003891
 featurewise_mean = 52.18
 
-print "Load saved model:", resume_from + '.',
+print "Load saved model:", resume_from + '.'
+
+model = load_model(resume_from)
+
 print "Done."
 
-patch_model = load_model(resume_from, compile=False)
-
-
 if image_path is None:
+    num_samples = 0
+    num_correct = 0
     image_dir = "data/curated_breast_imaging_ddsm/Combined_full_images/full_test_1152x896/pos"
-    for image_file_name in os.path.list_dir(image_dir):
+    for image_file_name in os.listdir(image_dir):
         image_path = os.path.join(image_dir, image_file_name)
         image = read_img_for_pred(
             image_path,
@@ -38,8 +41,6 @@ if image_path is None:
             rescale_factor=rescale_factor
         )
 
-        print image.mean(), "+-", image.std()
-
         # image = read_resize_img(
         #     ARGS.image_path,
         #     (1152, 896),
@@ -47,8 +48,22 @@ if image_path is None:
         #     gs_255=False
         # )
 
+        print "On reading", image.mean(), "+-", image.std()
+
         image -= featurewise_mean
 
-        pred = image.predict(image)
+        print "On centering", image.mean(), "+-", image.std()
 
-        print "prediction:", pred
+        images = image[None, ...]
+        probs = model.predict(images)
+        class_ = np.argmax(probs[0])
+
+        print "prediction:", class_
+        print ""
+
+        num_samples += 1
+        if class_ == 1:
+            num_correct += 1
+
+    print num_correct, "/", num_samples
+    print "Total accuracy:", float(num_correct) / float(num_samples)
